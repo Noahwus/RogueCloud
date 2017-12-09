5991d40c9b2a27b4c1c66b9526e9cd76eb60cb64
Noahwus <noahwus@gmail.com>
2017-12-09 00:07:38 -0600

    Dungeon Gen
    Imported Script from tutorial, personalizing, making it fit for my project.
    
    +Moved Assets into Folders (textures, Scripts)
    - Scene folder come later

---
+++ Assets/Rogue Scripts/MapGeneration.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapGeneration : MonoBehaviour {
+	public int mapRows = 10;
+	public int mapColums = 10;
+
+	private char[,] map;
+
+	private string boxCharacters;
+	private string[] boxCharacterUpNeighbors;
+	private string[] boxCharacterDownNeighbors;
+	private string[] boxCharacterLeftNeighbors;
+	private string[] boxCharacterRightNeighbors;
+
+	void Start(){
+		InitializeBoxCharacters ();
+		//InititializeMap();
+		//DisplayMap();
+	}
+
+	private void InitializeBoxCharacters(){
+		boxCharacters = "";
+		boxCharacterUpNeighbors = new string[boxCharacters.Length];
+		boxCharacterDownNeighbors = new string[boxCharacters.Length];
+		boxCharacterLeftNeighbors = new string[boxCharacters.Length];
+		boxCharacterRightNeighbors = new string[boxCharacters.Length];
+
+		boxCharacterLeftNeighbors [0] = "";
+
+	}
+}
