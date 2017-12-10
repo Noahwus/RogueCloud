07e4c8860e04447e3bd1362a67bb30d9813c868f
Noahwus <noahwus@gmail.com>
2017-12-09 21:04:52 -0600

    Starting Random Gen
    This is fucking me. Need to implement more tetris T tiles which will add more work. Need to break for tonight

--- Assets/Rogue Scripts/MapGeneration.cs
+++ Assets/Rogue Scripts/MapGeneration.cs
@@ -21,13 +21,39 @@ public class MapGeneration : MonoBehaviour {
 	}
 
 	private void InitializeBoxCharacters(){
-		boxCharacters = "";
+		boxCharacters = "01234567";
 		boxCharacterUpNeighbors = new string[boxCharacters.Length];
 		boxCharacterDownNeighbors = new string[boxCharacters.Length];
 		boxCharacterLeftNeighbors = new string[boxCharacters.Length];
 		boxCharacterRightNeighbors = new string[boxCharacters.Length];
 
-		boxCharacterLeftNeighbors [0] = "";
+		boxCharacterLeftNeighbors [0] = "0246";
+		boxCharacterLeftNeighbors [1] = "1357";
+		boxCharacterLeftNeighbors [2] = "1X";
+		boxCharacterLeftNeighbors [3] = "026X";
+		boxCharacterLeftNeighbors [4] = "5X";
+		boxCharacterLeftNeighbors [5] = "046";
+		boxCharacterLeftNeighbors [6] = "0245X";
+		boxCharacterLeftNeighbors [7] = "17X";
+
+		boxCharacterRightNeighbors[0] = "0356";
+		boxCharacterRightNeighbors[1] = "17X";
+		boxCharacterRightNeighbors[2] = "06";
+		boxCharacterRightNeighbors[3] = "1X";
+		boxCharacterRightNeighbors[4] = "06";
+		boxCharacterRightNeighbors[5] = "1X";
+		boxCharacterRightNeighbors[6] = "035X";
+		boxCharacterRightNeighbors[7] = "1X";
+
+		boxCharacterUpNeighbors[0] = "0X";
+		boxCharacterUpNeighbors[1] = "";
+		boxCharacterUpNeighbors[2] = "";
+		boxCharacterUpNeighbors[3] = "";
+		boxCharacterUpNeighbors[4] = "";
+		boxCharacterUpNeighbors[5] = "";
+		boxCharacterUpNeighbors[6] = "";
+		boxCharacterUpNeighbors[7] = "";
+
 
 	}
 }
